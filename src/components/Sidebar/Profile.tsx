d7c1a805f1688556928c2f5d2b009db6e533e1a0
Arthur Pedroti <arthurpedroti@gmail.com>
2023-09-14 18:12:25 -0300

    sidebar

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,24 @@
+import { LogOut } from 'lucide-react'
+
+export function Profile() {
+  return (
+    <div className="grid items-center gap-3 grid-cols-profile">
+      <img
+        src="https://github.com/ArthurPedroti.png"
+        className="w-10 h-10 rounded-full"
+        alt=""
+      />
+      <div className="flex flex-1 flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">
+          Arthur Pedroti
+        </span>
+        <span className="text-sm text-zinc-500 truncate">
+          arthurpedroti@gmail.com
+        </span>
+      </div>
+      <button type="button" className="ml-auto p-2 hover:bg-zinc-50 rounded-md">
+        <LogOut className="w-5 h-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
